3a36fde08155ba7a5e15d0edad138b7fbfa263bf
Mahadi9990 <mdshafayat9999@gmail.com>
2025-07-31 07:28:08 +0600

    sry to make for loop update while loop

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 console.log('This is the index.js file');
 
-for(i=0; i <60 ;i++) {
+// commitment.js 
+let i =0;
+while( i <60) {
+    i++
     console.log(` ${i} I will invest at least 6 hrs every single day for next 60 days`);
 }
+
+// 2 odd even.js
 
