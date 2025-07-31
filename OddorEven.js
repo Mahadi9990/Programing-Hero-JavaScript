d683a966a2e15fb356bb7451e88b3e4f39c8a97b
Mahadi9990 <mdshafayat9999@gmail.com>
2025-07-31 07:43:32 +0600

    add while even & odd

---
+++ OddorEven.js
@@ -0,0 +1,17 @@
+// 2 odd even.js
+
+let odd = 61;
+
+while(odd < 100){
+    odd = odd+2
+    console.log(`Odd number ${odd}`)
+}
+
+// even number
+
+let even = 78;
+
+while(even < 98){
+    even = even+2
+    console.log(`Even number ${even}`)
+}
---
+++ textRepeat.js
@@ -0,0 +1,11 @@
+console.log('This is the index.js file');
+
+// commitment.js 
+let textRepeat =0;
+while( textRepeat <60) {
+    textRepeat++
+    console.log(` ${textRepeat} I will invest at least 6 hrs every single day for next 60 days`);
+}
+
+
+
