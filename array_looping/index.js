f8d9a897a66a7270bb1ceba5fae60a40a7f819a5
Mahadi9990 <mdshafayat9999@gmail.com>
2025-08-06 09:25:11 +0600

    Use a for...of loop to concatenate all the elements of an array into a single string.

--- array_looping/index.js
+++ array_looping/index.js
@@ -1,7 +1,6 @@
 // const colors = ['red', 'blue', 'green', 'yellow', 'orange']
 // let colors2 = []
 // let colors3 = []
-
 // for( i = colors.length - 1 ; i >= 0 ; i--){
 //     colors2.push(colors[i])
 //     colors3.unshift(colors[i])
@@ -10,13 +9,20 @@
 // console.log(colors3)
 
 
-const numbers = [12, 98, 5, 41, 23, 78, 46];
-const evenNumber =[]
+// const numbers = [12, 98, 5, 41, 23, 78, 46];
+// const evenNumber =[]
+// for( i = 0 ; i <= numbers.length; i++){
+//     if( numbers[i] % 2 === 0){
+//         evenNumber.push(numbers[i])
+//     }
+// }
+// console.log(evenNumber)
 
-for( i = 0 ; i <= numbers.length; i++){
-    if( numbers[i] % 2 === 0){
-        evenNumber.push(numbers[i])
-    }
-}
- 
-console.log(evenNumber)
+// var numbers = ['Tom', 'Tim', 'Tin', 'Tik']
+// var all =""
+// for( let item of numbers){
+//     all += item 
+// }
+// console.log(all)
+
+
